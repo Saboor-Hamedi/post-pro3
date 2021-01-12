a5e5f5be1049d1315a7a1a4ab97637d13291505f
unknown <janhamedi49@gmail.com>
2021-01-12 02:59:41 -0500

    Initial

--- js/main.js
+++ js/main.js
@@ -6,36 +6,40 @@ menuBtn.addEventListener('click', function () {
      menuBar.classList.toggle('active');
 }); 
 
+// keep page
+    $(document).ready(function(){
+        $(this).scrollTop(0);
+    });
 
 //   dard mode
-$('.main-mode').toggleClass(localStorage.toggled);
+    $('.main-mode').toggleClass(localStorage.toggled);
     function darkLight() {
+        location.reload();
     /*DARK CLASS*/
     if (localStorage.toggled != 'dark') {
-        $('.main-mode, p').toggleClass('dark', true);
-        localStorage.toggled = "dark";
+    $('.main-mode, p').toggleClass('dark', true);
+    localStorage.toggled = "dark";
     } else {
-        $('.main-mode, p').toggleClass('dark', false);
-        localStorage.toggled = "";
-    }
+    $('.main-mode, p').toggleClass('dark', false);
+    localStorage.toggled = "";
     }
 
-    /*Add 'checked' property to input if background == dark*/
+    }
+/*Add 'checked' property to input if background == dark*/
     if ($('.main-mode').hasClass('dark')) {
     $( '#checkBox' ).prop( "checked", true )
     } else {
     $( '#checkBox' ).prop( "checked", false )
     }
-
-
+    
+// SCROLL
     $('a[href*="#"]').on('click', function (e) {
-        e.preventDefault()
-      
-        $('html, body').animate(
-          {
-            scrollTop: $($(this).attr('href')).offset().top,
-          },
-          500,
-          'linear'
-        )
-      })
+    e.preventDefault()
+    $('html, body').animate(
+    {
+    scrollTop: $($(this).attr('href')).offset().top,
+    },
+    500,
+    'linear'
+    )
+    })
